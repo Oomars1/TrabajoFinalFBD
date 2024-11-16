2bc1df49d02e05596947aa0d5ffde9c00feb0e0e
mp19034 <mp19034@ues.edu.sv>
2024-11-15 19:48:21 -0600

    se agrego requeriments

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 #Creamos la conexion ala base de datos
-#engine=create_engine("mariadb+pymysql://root:oomars2401@localhost:3306/pruebagas")
-engine=create_engine("mysql://ladybug:rI8]xG2*sM0[qO4&@yabbering-scarlet-herring-snbwu-mariadb.yabbering-scarlet-herring-snbwu.svc.cluster.local:3306/yabbering-scarlet-herring")
+engine=create_engine("mariadb+pymysql://root:oomars2401@localhost:3306/pruebagas")
+#engine=create_engine("mysql://ladybug:rI8]xG2*sM0[qO4&@yabbering-scarlet-herring-snbwu-mariadb.yabbering-scarlet-herring-snbwu.svc.cluster.local:3306/yabbering-scarlet-herring")
 #creamos la metadata para la creacion de los datos
 meta=MetaData()
 #almacenamos la conexion para utilizarla
