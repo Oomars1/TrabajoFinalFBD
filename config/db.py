379df490b5037ebc402b152d44d3f17de9955747
mp19034 <mp19034@ues.edu.sv>
2024-11-15 18:39:59 -0600

    prueba db

--- config/db.py
+++ config/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, MetaData
 
 # Crear la conexión a la base de datos
 engine = create_engine(
-    "mysql://edwin:oomars2401@/pruebagas"
+    "mysql://edwin:oomars2401@pruebagas-ibqod-mariadb.pruebagas-ibqod.svc.cluster.local:3306/pruebagas"
 )
 
 # Crear la metadata para la creación de los datos
