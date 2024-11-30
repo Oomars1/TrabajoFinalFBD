719559d3e536d45aeaf107e2851af2a6061f7244
mp19034 <mp19034@ues.edu.sv>
2024-11-29 18:02:31 -0600

    Se corrigio errores de app y procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn app:app --host 0.0.0.0 --port 10000
+web: gunicorn -w 4 -b 0.0.0.0:10000 app:app -k uvicorn.workers.UvicornWorker
--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes.user import user
-#utilizamos el modulo de fastAPI
 from config.openapi import tags_metadata
 
-
-app=FastAPI(
-   title="Users API",
-   description="a REST API using python and mysql",
-   version="0.0.1",
-   openapi_tags=tags_metadata,
+app = FastAPI(
+    title="Users API",
+    description="A REST API using Python and MySQL",
+    version="0.0.1",
+    openapi_tags=tags_metadata,
 )
+
+@app.get("/favicon.ico")
+async def favicon():
+    return {"message": "No favicon available"}
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Users API"}
 
 app.include_router(user)
