f49d36a3c08013ea30599159c2333b94868ce670
mp19034 <mp19034@ues.edu.sv>
2024-11-29 18:26:07 -0600

    Se corrigio errores de app y procfile

--- app.py
+++ app.py
@@ -3,18 +3,13 @@ from fastapi.staticfiles import StaticFiles
 from routes.user import user
 from config.openapi import tags_metadata
 
-app = FastAPI(
-    title="Users API",
-    description="A REST API using Python and MySQL",
-    version="0.0.1",
-    openapi_tags=tags_metadata,
-)
+app = FastAPI()
 
 @app.get("/favicon.ico")
 async def favicon():
     return {"message": "No favicon available"}
 
-@app.get("/")
+@app.get("/docs")
 def read_root():
     return {"message": "Welcome to the Users API"}
 
